15425518a9521f9f13ab0d0ff544103d80ffe059
Hugo <hugo_martins@cepedi.org.br>
2025-09-11 18:02:24 -0300

    Faltando somente o a ação do servo

--- main.c
+++ main.c
@@ -14,12 +14,21 @@
 //Botões
 #define bot_a 5
 #define bot_b 6
+#define bot_c 22 // Esse botão será utilizado para simular o pressionar de botão do ticket do estacionamento.
+bool manual = 0;
+bool abrir = 0;
 
-//PWM
+/*PWM
+Som*/
 #define buzzer_a 21
-#define div 100.0f
-#define wrap 24999 // div + wrap = 50HZ
-uint32_t slice = 0;
+#define div_buzz 100.0f
+#define wrap_buzz 2840
+uint32_t slice_buzz = 0;
+//Servo
+#define servo 16
+#define div_servo 100.0f
+#define wrap_servo 24999 // div + wrap = 50HZ
+uint32_t slice_servo = 0;
 
 //i2c
 #define I2C_PORT i2c1
@@ -27,34 +36,46 @@ uint32_t slice = 0;
 #define I2C_SCL 15
 #define endereco 0x3C
 
-//Controle Servomotor
-#define velocidade_servo 17
-#define direcao_servo 16
-
 //Oled
 ssd1306_t ssd;
+char str_permissao[10];
+uint16_t carros = 0;
+char str_estado[10];
+char str_limite[10] = {"400 = MAX"};
+
+
+//Interrupção
+#define interrupcoes(bots) gpio_irq_set_enabled_with_callback(bots, GPIO_IRQ_EDGE_FALL, true, &irq_gpio_handler);
 
 void core1(void);
 void led_init(uint8_t led);
 void bot_init(void);
-void pwm_setup(void);
+void irq_gpio_handler(uint32_t evets, uint gpio);
+void pwm_setup(uint32_t slice, float div, uint32_t wrap, uint8_t pin);
+void sirene(uint8_t pin, uint32_t wrap, uint8_t i);
 void i2c_setup(void);
-void oled_init();
+void oled_init(void);
+void oled_run(void); //display oled apresentando os valores
+void passagem(void); //lógica do loop principal
 
 
 int main(){
-    stdio_init_all();
     multicore_launch_core1(core1);
     led_init(green_led);
     led_init(red_led);
     bot_init();
+    pwm_setup(slice_buzz, div_buzz, wrap_buzz, buzzer_a);
+    pwm_setup(slice_servo, div_servo, wrap_servo, servo);
 
-    watchdog_enable(5000, 1);
+    watchdog_enable(12000, 1);
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+        if(!manual){
+            passagem();
+        }
+    }        
+        sleep_ms(500);
 }
+
 
 //Função do core1 do MCU
 void core1(void){
@@ -74,19 +95,57 @@ void led_init(uint8_t led){
 }
 
 void bot_init(void){
-    for(uint8_t i = 5; i < 7; i++){
-        gpio_init(i);
-        gpio_set_dir(i, GPIO_IN);
-        gpio_pull_up(i);
+    uint8_t botoes[2] = {bot_a, bot_b, bot_c};
+    for(uint8_t i = 0; i < 3; i++){
+        gpio_init(botoes[i]);
+        gpio_set_dir(botoes[i], GPIO_IN);
+        gpio_pull_up(botoes[i]);
     }
 }
 
-void pwm_setup(void){
-    gpio_set_function(buzzer_a, GPIO_FUNC_PWM);
-    slice = pwm_gpio_to_slice_num(buzzer_a);
+void irq_gpio_handler(uint32_t evets, uint gpio){
+    uint64_t current_time = to_ms_since_boot(get_absolute_time());
+    static uint64_t last_time_a = 0, last_time_b = 0;
+    if(gpio == bot_a && (current_time - last_time_a > 300)){
+        if(!manual){
+            sirene(buzzer_a, wrap_buzz, 1);
+            manual = 1;
+            manual ? sprintf(str_estado, "Manual") : sprintf(str_estado, "Automatico");
+        } else {
+            sirene(buzzer_a, wrap_buzz, 2);
+            manual = 0;
+            manual ? sprintf(str_estado, "Manual") : sprintf(str_estado, "Automatico");
+        }
+
+    } else if ((gpio == bot_b && manual) && (current_time - last_time_b > 300)){
+        if(abrir){
+            //função pra fechar
+            abrir = 0;
+            abrir ? sprintf(str_permissao, "PERMITIDO") : sprintf(str_permissao, "NEGADO");
+
+        } else {
+            //função pra abrir
+            abrir = 1;
+            abrir ? sprintf(str_permissao, "PERMITIDO") : sprintf(str_permissao, "NEGADO");
+        }
+    } 
+}
+
+void pwm_setup(uint32_t slice, float div, uint32_t wrap, uint8_t pin){
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    slice = pwm_gpio_to_slice_num(pin);
     pwm_set_clkdiv(slice, div);
     pwm_set_wrap(slice, wrap);
     pwm_set_enabled(slice, true);
+}
+
+void sirene(uint8_t pin, uint32_t wrap, uint8_t quantidade){
+    for(uint8_t i; i < quantidade; i++){
+        pwm_set_gpio_level(pin, wrap);
+        sleep_ms(100);
+        pwm_set_gpio_level(pin, 0);
+        sleep_ms(100);
+    }
 }
 
 void i2c_setup(void){
@@ -106,8 +165,32 @@ void oled_init(void){
 
 void oled_run(void){
     ssd1306_draw_string(&ssd, "Estacionamento", 0, 0);
-    /*ssd1306_draw_string(&ssd, , 1, 0);
-    ssd1306_draw_String(&ssd, , 2, 0);
-    ssd1306_draw_String(&ssd, , 3, 0);*/
+    ssd1306_draw_string(&ssd, str_permissao, 1, 0);
+    if(carros < 400) {
+        ssd1306_draw_String(&ssd, carros, 2, 0);
+    } else{
+        ssd1306_draw_String(&ssd, "400 = MAX", 2, 0);    
+    }
+    ssd1306_draw_String(&ssd, str_estado, 3, 0);
     sleep_ms(50);
+}
+
+void passagem(void){
+    uint64_t current_time = to_ms_since_boot(get_absolute_time());
+    static uint64_t last_time = 0;
+    if(bot_c && (current_time - last_time >300)){
+        if(carros < 400){
+            sleep_ms(2000);
+            carros++;
+            abrir = 1;
+            sprintf(str_permissao, "PERMITIDO");
+            gpio_put(red_led, 0);
+            gpio_put(green_led, 1);
+            sleep_ms(5000); // Este sleep é pra simular o sensor que detecta a passagem do carro.
+            abrir = 0;
+            gpio_put(green_led, 0);
+            gpio_put(red_led, 1);
+            sprintf(str_permissao, "NEGADO");
+        } 
+    } 
 }
